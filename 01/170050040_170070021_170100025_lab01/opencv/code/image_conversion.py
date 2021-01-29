4d6bd5d21da61e331c5695b6f642c8344c180fb2
Siddharth Saha <sahasiddharth611@gmail.com>
2021-01-29 23:19:09 +0530

    Added save feature in opencv code

--- 01/170050040_170070021_170100025_lab01/opencv/code/image_conversion.py
+++ 01/170050040_170070021_170100025_lab01/opencv/code/image_conversion.py
@@ -4,6 +4,8 @@ import cv2
 import matplotlib.pyplot as plt
 import sys
 
+save_dir = "../results/"
+
 bgr_img = cv2.imread(sys.argv[1])
 rgb_img = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2RGB)
 normalized_bgr = bgr_img/255.
@@ -17,9 +19,12 @@ plt.title("Original image")
 f.add_subplot(1, 2, 2)
 plt.title("Normalized image")
 plt.imshow(normalized_rgb)
+plt.savefig(save_dir + "matplotlib.png")
 plt.show()
 
 
 cv2.imshow("Original image", bgr_img)
-cv2.imshow("Normalized image", normalized_rgb)
+cv2.imwrite(save_dir + "cv2_original.png", bgr_img)
+cv2.imshow("Normalized image", normalized_bgr)
+cv2.imwrite(save_dir + "cv2_normalized.png", normalized_bgr)
 cv2.waitKey(0)
