98d474bbc71e0c1a478187ccab2d75bdd739b0f5
parthisultimate <parthisultimate@gmail.com>
2021-03-02 11:29:25 +0530

    2.2 updated

--- 04/130010009_140076001_150050001_lab03_midsem/code/pano_manual.py
+++ 04/130010009_140076001_150050001_lab03_midsem/code/pano_manual.py
@@ -5,24 +5,60 @@ import os
 import sys
 from os import listdir
 from os.path import isfile, join
+
 images = [f for f in listdir(sys.argv[1]) if isfile(join(sys.argv[1], f))]
 images.sort()
 img_p1 = sys.argv[1] + "/" + images[0]
 img_p2 = sys.argv[1] + "/" + images[1]
+
+if(sys.argv[1].split("/")[-1]=="campus" or sys.argv[1].split("/")[-1]=="yard"):
+    temp = img_p1
+    img_p1 = img_p2
+    img_p2 = temp
+
 img1 = cv2.imread(img_p1)
 img2 = cv2.imread(img_p2)
-res = img1.shape[0]/480
+res = img1.shape[0]/600
+img1_points = []
+img2_points = []
 
-resized1 = cv2.resize(img1, (int(img1.shape[1]/res),480) , interpolation = cv2.INTER_AREA)
-resized2 = cv2.resize(img2, (int(img2.shape[1]/res),480) , interpolation = cv2.INTER_AREA)
+    
+def coordinates_click(event, x, y, flags, params): 
+  
+    if event == cv2.EVENT_LBUTTONDOWN and params=="image1": 
+        img1_points.append([x,y])
+    elif event == cv2.EVENT_LBUTTONDOWN and params=="image2":
+        img2_points.append([x,y])    
+
+
+cv2.namedWindow('image1',cv2.WINDOW_NORMAL)
+cv2.namedWindow('image2',cv2.WINDOW_NORMAL)
+
+cv2.resizeWindow('image1', (min(int(img1.shape[1]/res),600),600))
+cv2.resizeWindow('image2', (min(int(img1.shape[1]/res),600),600))
+
+cv2.imshow('image1', img1)
+cv2.imshow('image2', img2) 
+
+cv2.moveWindow('image1',0,200)
+cv2.moveWindow('image2',min(int(img1.shape[1]/res),600)+100,230)
+
+cv2.setMouseCallback('image1', coordinates_click, "image1") 
+cv2.setMouseCallback('image2', coordinates_click, "image2") 
+
+cv2.waitKey(0) 
+cv2.destroyAllWindows()
+
+img1_points = np.asarray(img1_points,dtype = np.float32)
+img2_points = np.asarray(img2_points,dtype = np.float32)
 
 
 #campus
 # img2_points = np.float32([[126,313],[89,64],[176,47],[122,49],[30,111]])
 # img1_points = np.float32([[392,318],[347,71],[432,47],[379,53],[290,121]])
 #society
-img1_points = res*np.float32([[140,103],[140,390],[192,130],[160,116],[158,153],[216,135]])
-img2_points = res*np.float32([[39,86],[35,378],[91,119],[60,102],[57,140],[112,126]])
+# img1_points = res*np.float32([[140,103],[140,390],[192,130],[160,116],[158,153],[216,135]])
+# img2_points = res*np.float32([[39,86],[35,378],[91,119],[60,102],[57,140],[112,126]])
 #yard
 # img2_points = res*np.float32([[338,266],[366,143],[131,297],[286,137],[193,237]])
 # img1_points = res*np.float32([[549,275],[573,144],[342,297],[487,143],[397,243]])
@@ -39,11 +75,19 @@ offset = np.array([[ 1 , 0 , 0],
 [ 0 , 1 , int(img1.shape[0]/2)],
 [ 0 , 0 ,    1    ]])
 matrix = offset@matrix
-dst = cv2.warpPerspective(img2,matrix,(img1.shape[1]+img2.shape[1], int(img1.shape[0]*2)))
-res_final = dst.shape[0]/400
-dst2 = dst.copy()
+mosaic = cv2.warpPerspective(img2,matrix,(img1.shape[1]*3, int(img1.shape[0]*3)))
 
-dst2[int(img1.shape[0]/2):img1.shape[0]+int(img1.shape[0]/2), :img1.shape[1]] = img1
-resized = cv2.resize(dst2, (int(dst.shape[1]/res_final), 400) , interpolation = cv2.INTER_AREA)
+mosaic[int(img1.shape[0]/2):img1.shape[0]+int(img1.shape[0]/2), :img1.shape[1]] = img1
+img_gray = cv2.cvtColor(mosaic, cv2.COLOR_BGR2GRAY)
+xx,yy = np.where(img_gray!=0)
+max_X = max(xx)
+min_X = min(xx)
+max_Y = max(yy)
+min_Y = min(yy)
+mosaic = mosaic[min_X:max_X,min_Y:max_Y]
+res_final = mosaic.shape[0]/400
+resized = cv2.resize(mosaic, (int(mosaic.shape[1]/res_final), 400) , interpolation = cv2.INTER_AREA)
 cv2.imshow("Final",resized)
 cv2.waitKey(0)
+pathk = "../results/pano-manual-results/House/fi_House.jpg"
+cv2.imwrite(pathk,resized)
