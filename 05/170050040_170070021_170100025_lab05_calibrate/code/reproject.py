a962d7ba68ce91d9b7dcbd747a3936f443a52778
Parikshit Bansal <parikshitbansal@Parikshits-MacBook-Air.local>
2021-03-14 01:21:11 +0530

    done parikshit

--- 05/170050040_170070021_170100025_lab05_calibrate/code/reproject.py
+++ 05/170050040_170070021_170100025_lab05_calibrate/code/reproject.py
@@ -1,102 +1,36 @@
 import numpy as np
 import cv2
-import argparse
+import argparse,copy
+
+
+def sort(arr):
+    # hardcoded for now
+    arr = arr[np.argsort(arr[:,1])]
+    arr[:3] = arr[:3][np.argsort(arr[:3,0])]
+    arr[3:] = arr[3:][np.argsort(arr[3:,0])]
+    return arr
+
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-f", action="store", dest="filepath")
+parser.add_argument("-f", action="store")
 args = parser.parse_args()
+image_shape = (1000, 1000)
+arr_order = np.array([[0, 0, 0], [1, 0, 0], [2, 0, 0], [0, 1, 0], [1, 1, 0], [2, 1, 0]],dtype=np.float32)
+objpoints = np.repeat(arr_order[None,:,:],2,axis=0).astype(np.float32)
 
+points = (np.loadtxt(args.f)).astype(np.float)
+pts1 = points[:6,:]
+pts2 = points[6:12,:]
+imgpoints = np.array([sort(pts1),sort(pts2)], dtype=np.float32)
 
-# Function to rearrange points in the order we need
-# to be consistent with order in objpoints array
-def rearrange(l):
-    l1 = []
-    l2 = []
-    l3 = []
-    maxx = 0
-    minx = 10000
-    for i in range(6):
-        if (maxx<l[i][0]):
-            maxx = l[i][0]
-        elif (minx > l[i][0]):
-            minx = l[i][0]
-        else: 
-            pass
-    for i in range(6):
-        if (abs(l[i][0]-maxx)<5):
-            l3.append(l[i])
-        elif (abs(minx - l[i][0])<5):
-            l1.append(l[i])
-        else:
-            l2.append(l[i])
-    if (l1[0][1] > l1[1][1]):
-        l[5] = l1[0]
-        l[2] = l1[1]
-    else:
-        l[5] = l1[1]
-        l[2] = l1[0]
-    if (l2[0][1] > l2[1][1]):
-        l[4] = l2[0]
-        l[1] = l2[1]
-    else:
-        l[4] = l2[1]
-        l[1] = l2[0]
-    if (l3[0][1] > l3[1][1]):
-        l[3] = l3[0]
-        l[0] = l3[1]
-    else:
-        l[3] = l3[1]
-        l[0] = l3[0]
-    return l
-
-
-f = open(args.filepath,'r')
-pts = f.readlines()
-for i in range(len(pts)):
-    pts[i] = pts[i][:-1]
-    pts[i] = pts[i].split(' ')
-    pts[i][0] = float(pts[i][0])
-    pts[i][1] = float(pts[i][1])
-
-pts1 = pts[:6]
-pts2 = pts[6:12]
-pm1 = rearrange(pts1)
-pm2 = rearrange(pts2)
-
-print("Rearranged points for image 1 are")
-print(pm1)
-print("Rearranged points for image 2 are")
-print(pm2)
-
-objpoints = np.array([[[0, 0, 0], [3, 0, 0], [6, 0, 0], [0, 3, 0], [3, 3, 0], [6, 3, 0]],
-                      [[0, 0, 0], [3, 0, 0], [6, 0, 0], [0, 3, 0], [3, 3, 0], [6, 3, 0]]], dtype = np.float32)
-
-# Image points in sequence corresponding to above
-imgpoints = np.array([pm1, pm2], dtype=np.float32)
-
-print (objpoints.shape)
-print (imgpoints.shape)
-exit()
 ret, mtx, dist, rvecs, tvecs = \
     cv2.calibrateCamera(objpoints, imgpoints,
-                        (720, 1280), None, None, flags=
-                         (cv2.CALIB_ZERO_TANGENT_DIST + cv2.CALIB_FIX_K1 + cv2.CALIB_FIX_K2
-                          + cv2.CALIB_FIX_K3 + cv2.CALIB_FIX_K4 + cv2.CALIB_FIX_K5 + cv2.CALIB_FIX_K6))
+                        image_shape, None, None)
 
-print("Learned/Fitted Intrinsic matrix is")
-print(mtx)
-print("Rotation vectors are")
-print(rvecs)
-print("Translation vectors are")
-print(tvecs)
+mean_error = 0
+for i in range(len(imgpoints)):
+    imgpoints2, _ = cv2.projectPoints(arr_order, rvecs[i], tvecs[i], mtx, dist)
+    error = cv2.norm(imgpoints[i],imgpoints2[:,0,:], cv2.NORM_L2)/len(imgpoints2)
+    mean_error += error
 
-projected0, _ = cv2.projectPoints(objpoints[0], rvecs[0], tvecs[0], mtx, dist)
-projected1, _ = cv2.projectPoints(objpoints[1], rvecs[1], tvecs[1], mtx, dist)
-
-# Reproject points
-projected0 = projected0.reshape(1, 6, 2)
-projected1 = projected1.reshape(1, 6, 2)
-error0 = np.sqrt(np.sum(np.square(imgpoints[0]-projected0)))/12
-error1 = np.sqrt(np.sum(np.square(imgpoints[1]-projected1)))/12
-
-print("Average projection error {0:.2f}".format(error0+error1))
+print (mean_error/len(objpoints))
