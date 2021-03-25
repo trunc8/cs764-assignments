38fef7abe7fbd2156f106bf7cbc58b69a0ecd3ff
parthisultimate <parthisultimate@gmail.com>
2021-03-25 13:28:43 +0530

    gg

--- 06/170050040_170070021_170100025_lab06_AR/code/augment_book.py
+++ 06/170050040_170070021_170100025_lab06_AR/code/augment_book.py
@@ -11,9 +11,9 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-w', default=1.5)
 parser.add_argument('-l', default=5)
 parser.add_argument('-b', default=3)
-parser.add_argument('-x', default=60)
-parser.add_argument('-y', default=40)
-parser.add_argument('-theta', default=45)
+parser.add_argument('-x', default=1000)
+parser.add_argument('-y', default=1000)
+parser.add_argument('-theta', default=1000)
 
 args = parser.parse_args()
 
@@ -24,14 +24,27 @@ x = int(args.x)
 y = int(args.y)
 theta = int(args.theta)
 theta_rad = (theta*math.pi)/180
+
+
 def draw_book_on_img(image, projects):
-    projects = np.array(projects,np.int32)
-    projects = projects.reshape(8,1,2)
-    image = cv2.drawContours(image, [projects[:4]], -1, (0, 0, 255), 5)
-    print(projects[1])
+    centre_old = (projects[0] +projects[2])/2
+    
+    new_x = (x*image.shape[1])/100
+    new_y = (y*image.shape[0])/100
+    distx = new_x - centre_old[0][0]
+    disty = new_y - centre_old[0][1]
+  
+    new_projects = projects.copy()
+    new_projects[:,:,0] = projects[:,:,0] + distx * (x!=1000)
+    new_projects[:,:,1] = projects[:,:,1] + disty * (y!=1000)
+   
+    
+    new_projects = np.array(new_projects,np.int32)
+    new_projects = new_projects.reshape(8,1,2)
+    image = cv2.drawContours(image, [new_projects[:4]], -1, (0, 0, 255), 5)
     for i in range(4):
-        image = cv2.line(image, tuple(projects[i][0]), tuple(projects[i+4][0]), (0, 0, 255), 5)
-    image = cv2.drawContours(image, [projects[4:]], -1, (0, 0, 255), 5)
+        image = cv2.line(image, tuple(new_projects[i][0]), tuple(new_projects[i+4][0]), (0, 0, 255), 5)
+    image = cv2.drawContours(image, [new_projects[4:]], -1, (0, 0, 255), 5)
  
     return image
 
@@ -40,7 +53,6 @@ def get_data():
   images = [f for f in os.listdir(directory) if 
           os.path.isfile(os.path.join(directory,f))]
   images.sort()
-  print(images)
   NUMBER_OF_WALL_IMAGES = 6
   front_path = os.path.join(directory, images[NUMBER_OF_WALL_IMAGES])
   front = cv2.imread(front_path)
@@ -48,7 +60,6 @@ def get_data():
   side = cv2.imread(side_path)
   wall_path = os.path.join(directory, images[0])
   wall = cv2.imread(wall_path)
-  print(front_path,"gggggg")
   # cv2.imshow("front", front)
   # cv2.imshow("side", side)
   # cv2.waitKey(0)
@@ -95,18 +106,18 @@ def coordinates_click(event, x, y, flags, params):
 # cv2.namedWindow('image6',cv2.WINDOW_NORMAL)
 
 # cv2.resizeWindow('image1', (min(int(img[0].shape[1]/res),600),600))
-# cv2.resizeWindow('image2', (min(int(img1.shape[1]/res),600),600))
-# cv2.resizeWindow('image3', (min(int(img1.shape[1]/res),600),600))
-# cv2.resizeWindow('image4', (min(int(img1.shape[1]/res),600),600))
-# cv2.resizeWindow('image5', (min(int(img1.shape[1]/res),600),600))
-# cv2.resizeWindow('image6', (min(int(img1.shape[1]/res),600),600))
+# cv2.resizeWindow('image2', (min(int(img[0].shape[1]/res),600),600))
+# cv2.resizeWindow('image3', (min(int(img[0].shape[1]/res),600),600))
+# cv2.resizeWindow('image4', (min(int(img[0].shape[1]/res),600),600))
+# cv2.resizeWindow('image5', (min(int(img[0].shape[1]/res),600),600))
+# cv2.resizeWindow('image6', (min(int(img[0].shape[1]/res),600),600))
 
-cv2.imshow('image1', img[0])
-# cv2.imshow('image2', img2)
-# cv2.imshow('image3', img3)
-# cv2.imshow('image4', img4)
-# cv2.imshow('image5', img5)
-# cv2.imshow('image6', img6)
+# cv2.imshow('image1', img[0])
+# cv2.imshow('image2', img[1])
+# cv2.imshow('image3', img[2])
+# cv2.imshow('image4', img[3])
+# cv2.imshow('image5', img[4])
+# cv2.imshow('image6', img[5])
 
 # cv2.setMouseCallback('image1', coordinates_click, "image1") 
 # cv2.setMouseCallback('image2', coordinates_click, "image2") 
@@ -115,28 +126,17 @@ cv2.imshow('image1', img[0])
 # cv2.setMouseCallback('image5', coordinates_click, "image5") 
 # cv2.setMouseCallback('image6', coordinates_click, "image6") 
 
-cv2.waitKey(0)
-
-pts = np.array([[25, 70], [25, 160], 
-                [110, 200], [200, 160], 
-                [200, 70], [110, 20]],
-               np.int32)
-  
-# pts = pts.reshape((-1, 1, 2))
-# print([pts])
-# img[1] = np.array(img[1],dtype = np.float32)
-# imgmm = cv2.polylines(img[1], [pts], 
-#                       True, (0,254,255), 10)
+# cv2.waitKey(0)
 
 
-print(img1_points,img2_points,img3_points,img4_points,img5_points,img6_points)
+
+
 img_points = np.array([[[692, 2988], [960, 2955], [1185, 2921], [651, 2696], [901, 2679], [1135, 2654]],
- [[358, 2629], [576, 2671], [776, 2713], [434, 2429], [642, 2479], [851, 2512]],
+ [[734, 3005], [934, 2946], [1118, 2921], [701, 2838], [909, 2796], [1101, 2754]],
   [[567, 3038], [834, 3021], [1076, 3005], [559, 2779], [809, 2771], [1051, 2754]],
    [[818, 3055], [1043, 2980], [1243, 2913], [734, 2829], [960, 2771], [1160, 2704]],
     [[417, 2579], [626, 2671], [818, 2746], [525, 2404], [709, 2496], [901, 2579]],
      [[567, 2829], [759, 2821], [934, 2804], [567, 2637], [751, 2629], [926, 2612]]],dtype = np.float32)
-print(img_points.shape)   
 book_points_old = np.float32([[0, 0, 0], [0,b,0],[l,b,0],[l,0,0],[0,0,w],[0,b,w],[l,b,w],[l,0,w]])
 real_world_points = np.array([[[0,0,0],[3,0,0],[6,0,0],[0,3,0],[3,3,0],[6,3,0]],
 [[0,0,0],[3,0,0],[6,0,0],[0,3,0],[3,3,0],[6,3,0]],
@@ -147,32 +147,30 @@ real_world_points = np.array([[[0,0,0],[3,0,0],[6,0,0],[0,3,0],[3,3,0],[6,3,0]],
 
 ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(real_world_points, img_points, img[0].shape[1::-1], None, None)
 
-print(mtx)
 mtx = np.array(mtx)
 
 
-rt_matrix = [[math.cos(theta_rad),-math.sin(theta_rad),x],
-            [math.sin(theta_rad),math.cos(theta_rad),y],
+rt_matrix = [[math.cos(theta_rad),-math.sin(theta_rad),l/2],
+            [math.sin(theta_rad),math.cos(theta_rad),b/2],
             [0,0,1]]
 book_temp = book_points_old.copy()
 book_temp[:,2] = 1   
-book_temp[:,0] = book_temp[:,0] - x
-book_temp[:,1] = book_temp[:,1] - y
+book_temp[:,0] = book_temp[:,0] - l/2
+book_temp[:,1] = book_temp[:,1] - b/2
 
-print(book_temp)
-# print(book_points) 
+
         
 new_book = rt_matrix@np.transpose(book_temp)
 book_points = book_points_old.copy()
 book_points[:,:2] = np.transpose(new_book)[:,:2]
-print(book_points)
-print(new_book)
+
+
+book_points_old =  book_points*(theta!=1000) + book_points_old*(theta==1000)
 # new_book[]
 # project 3D points to image plane
 for p in range(len(img_points)):
     ret,rvecs, tvecs = cv2.solvePnP(real_world_points[p], img_points[p], mtx, None)
     projects, jac = cv2.projectPoints(book_points_old, rvecs, tvecs, mtx, None)
-    print(projects,"ggggggg") 
     im = draw_book_on_img(img[p], projects)
     cv2.namedWindow('gg',cv2.WINDOW_NORMAL)
 
